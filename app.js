573c2e4303d37af24f593cf1edb940d90819d605
Jairus Junio <jairusjunio27@gmail.com>
2024-07-11 17:31:05 +0800

    Uploading into my own repository.

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const apiKey = "Kh5gVQGfC0TJzPYg2qFZPHuQ6DYFOutr"; // Replace with your actual API key
+    const apiKey = "XI5yrUD1JxiNnAx0MSUyBXZWmYvdCQ1F"; // Replace with your actual API key
     const form = document.getElementById("cityForm");
     const weatherDiv = document.getElementById("weather");
+    const hourlyDiv = document.getElementById("hourly");
+
 
     form.addEventListener("submit", function(event) {
         event.preventDefault();
@@ -9,6 +11,7 @@ document.addEventListener("DOMContentLoaded", function() {
         getWeather(city);
     });
 
+    // Get Location Key sa Postman >> Uses the city name
     function getWeather(city) {
         const url = `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${apiKey}&q=${city}`;
 
@@ -28,6 +31,7 @@ document.addEventListener("DOMContentLoaded", function() {
             });
     }
 
+    // Get Current Location sa Postman >> Uses the city location ID instead of the city name itself
     function fetchWeatherData(locationKey) {
         const url = `http://dataservice.accuweather.com/currentconditions/v1/${locationKey}?apikey=${apiKey}`;
 
@@ -46,14 +50,66 @@ document.addEventListener("DOMContentLoaded", function() {
             });
     }
 
+    //Get Hourly Weather
+    function fetchHourlyForecast(locationKey){
+      const url = `http://dataservice.accuweather.com/forecasts/v1/hourly/1hour/${locationKey}?apikey=${apiKey}`;
+
+      fetch(url)
+          .then(response => response.json())
+          .then(data => {
+              if (data && data.length > 0) {
+                  displayHourlyWeather(data[0]);
+              } else {
+                  hourlyDiv.innerHTML = `<p>No weather data available.</p>`;
+              }
+          })
+          .catch(error => {
+              console.error("Error fetching weather data:", error);
+              hourlyDiv.innerHTML = `<p>Error fetching weather data.</p>`;
+          });
+
+
+    }
+
+    // Display fetchWeatherData()
     function displayWeather(data) {
-        const temperature = data.Temperature.Metric.Value;
-        const weather = data.WeatherText;
+      const dateTime = data.DateTime;
+      const epochDateTime = data.EpochTime;
+      const weatherIcon = data.WeatherIcon;
+      const iconPhrase = data.IconPhrase;
+      const temperature = data.Temperature.Metric.Value;
+      const weather = data.WeatherText;
         const weatherContent = `
             <h2>Weather</h2>
             <p>Temperature: ${temperature}°C</p>
+            <p>Epoch: ${epochDateTime}</p>
             <p>Weather: ${weather}</p>
+            <p>Date: ${dateTime}</p>
+            <p>Weather: ${weatherIcon}</p>
+            <p>Icon: ${iconPhrase}</p>
         `;
         weatherDiv.innerHTML = weatherContent;
     }
+
+    // Display fetchHourlyForecast()
+    function displayHourlyWeather(data) {
+        const dateTime = data.DateTime;
+        const epochDateTime = data.EpochDateTime;
+        const weatherIcon = data.WeatherIcon;
+        const iconPhrase = data.IconPhrase;
+        const temperature = data.Temperature.Metric.Value;
+        const weather = data.WeatherText;
+        const hourlyContent = `
+            <h2>Hourly</h2>
+            <p>Date: ${dateTime}</p>
+            <p>Epoch: ${epochDateTime}</p>
+            <p>Weather: ${weatherIcon}</p>
+            <p>Icon: ${iconPhrase}</p>
+            <p>Temperature: ${temperature}</p>
+            <p>Weather: ${weather}</p>
+        `;
+        hourlyDiv.innerHTML = hourlyContent;
+    }
+
+
 });
